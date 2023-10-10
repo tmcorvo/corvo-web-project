fab074ee7a64ed5ddb49b5e481eb11a7e3f1ae97
Corvo <dsnicola25@gmail.com>
2023-10-10 12:04:10 -0600

    Add background color, adjust buttons and add home page buttons, home page visuals updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ProductsComponent } from './products/products.component';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { AccountCreationComponent } from './account-creation/account-creation.component';
+import { AboutComponent } from './about/about.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -18,9 +20,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { AboutComponent } from './about/about.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-
 
 @NgModule({
  declarations: [
